a10bfe62685b3f1071d341dd16d083e01856c011
KateKoval <kovalchuk.pn@ucu.edu.ua>
2025-01-16 19:09:12 +0200

    Added code of bch, convolutional codes, humming and lrc

---
+++ ConvolutionalCoding.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Coder.h"
+
+#include "Variables.h"
+
+class ConvolutionalCoding
+{
+public:
+    static void printEncoded(const std::string &encoded);
+
+    std::string encode(const std::vector<bool> &message);
+
+    std::string encode(const std::string &message);
+};
---
+++ ViterbyDecoding.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include "TrellisNode.h"
+#include <set>
+
+#include "Variables.h"
+
+class ViterbiDecoding
+{
+    std::unordered_map<int, std::vector<int>> branches{{0, {0, 2}}, {1, {0, 2}}, {2, {1, 3}}, {3, {1, 3}}};
+    std::unordered_map<int, std::string> trellisBijectionToSymbol{{0, "00"}, {1, "01"}, {2, "10"}, {3, "11"}};
+
+    std::unordered_map<int, std::vector<std::string>> rules{{0, {"00", "11"}}, {1, {"11", "00"}}, {2, {"10", "01"}}, {3, {"01", "10"}}};
+
+    std::vector<std::vector<TrellisNode>> states;
+
+    std::set<int> includedStates{0};
+
+    // traceback path
+    std::string traceBackPath(const int &state);
+
+    std::string traceBackLowestErrorPath();
+
+    // trellis
+    void buildTrellisDiagram(const std::vector<bool> &encoded);
+
+    void calculateErrorsForPaths(const std::vector<bool> &encoded);
+
+    // utils
+    int findMinState(const std::vector<TrellisNode> &trellisColumn);
+    int nextNodeByRule(const std::string &node, const std::string &rule);
+    size_t hammingDistance(const std::string &a, const std::string &b);
+
+public:
+    std::string decode(const std::string &encoded);
+
+    std::string decode(const std::vector<bool> &encoded);
+
+    static void printDecoded(const std::string &path);
+};
